bfb3c1fd72aaac1aed9ebf217a673a8c5b60bdf1
pyramid-seph <pyramid-seph@users.noreply.github.com>
2022-05-02 23:01:09 -0500

    Revert "Adjust animatio timing"
    This reverts commit 3e55b6c0ff3d00c15b00dbbd9b71d105f28e6c4e.

--- scenes/_debug/DebugRoom.gd
+++ scenes/_debug/DebugRoom.gd
@@ -6,11 +6,4 @@ onready var player = $World/Player
 
 func _ready():
 	randomize()
-	yield(get_tree().create_timer(1.0, false), "timeout")
 	player.revive($World/StartPosition.position)
-
-
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_RIGHT and event.pressed:
-				player.start_timed_invincibility()
