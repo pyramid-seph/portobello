74d398b466e752bee18e1ae42c5a3ad0d6b8b0ee
pyramid-seph <pyramid-seph@users.noreply.github.com>
2022-05-19 21:22:48 -0500

    Fix bird x movement shared

--- scenes/pickups/Bird.gd
+++ scenes/pickups/Bird.gd
@@ -3,7 +3,10 @@ extends Area2D
 const SCORE_POINTS = 50
 const SPEED := 32.0
 const DIRECTION := Vector2(1, 1)
-const VELOCITY := SPEED * DIRECTION
+const MIN_STAMINA := 5
+const BASE_STAMINA := 20
+
+var velocity: Vector2 = SPEED * DIRECTION
 
 onready var viewport_size = get_viewport_rect().size
 onready var viewport_width = viewport_size.x
@@ -13,6 +16,11 @@ onready var min_pos_x = 0
 onready var max_pos_x = viewport_width - sprite_width
 
 
+func _ready() -> void:
+	if randi() % 2 == 0:
+		velocity.x *= -1
+
+
 func _process(delta) -> void:
 	_move(delta)
 	_autoremove()
@@ -20,8 +28,8 @@ func _process(delta) -> void:
 
 func _move(delta) -> void:
 	if position.x > max_pos_x or position.x < min_pos_x:
-		VELOCITY.x *= -1
-	position += VELOCITY * delta
+		velocity.x *= -1
+	position += velocity * delta
 
 
 func _autoremove() -> void:
@@ -30,7 +38,8 @@ func _autoremove() -> void:
 
 
 func pick_up(picker) -> void:
-	if picker.is_in_group("players"):
+	if picker.has_method("add_points_to_score"):
 		picker.add_points_to_score(SCORE_POINTS)
-		picker.add_stamina(randi() % 20 + 5)
+	if picker.has_method("add_stamina"):
+		picker.add_stamina(randi() % BASE_STAMINA + MIN_STAMINA)
 	queue_free()
