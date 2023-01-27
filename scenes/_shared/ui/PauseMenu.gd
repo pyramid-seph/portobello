663e503aa05554957f40b1209ad68066b989535b
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-01-26 21:37:09 -0600

    Get keyboard focus

--- scenes/_shared/ui/PauseMenu.gd
+++ scenes/_shared/ui/PauseMenu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 @onready var pause_label = %PauseLabel
-@onready var give_up_label = %GiveUpLabel
+@onready var give_up_button = %GiveUpButton
 @onready var pause_dialog = $PauseDialog
-@onready var autofire_checkbox := %AutofireCheckbox as CheckButton
-@onready var vibrate_checkbox := %VibrateCheckbox as CheckButton
+@onready var confirm_exit_dialog = $ConfirmExitLevelDialog
+@onready var autofire_checkbox := %AutofireCheckButton as CheckButton
+@onready var vibrate_checkbox := %VibrateCheckButton as CheckButton
+@onready var no_button := %NoButton
 
 @export var show_auto_fire: bool = false
 
@@ -21,8 +23,11 @@ func show_menu(make_visible: bool) -> void:
 	
 	visible = make_visible
 	if visible:
+		confirm_exit_dialog.visible = false
+		pause_dialog.visible = true
 		autofire_checkbox.visible = show_auto_fire
 		_populate_settings()
+		vibrate_checkbox.call_deferred("grab_focus")
 		# Hack? This resets its size to the height of its content.
 		pause_dialog.size.y = 0
 	else:
@@ -34,22 +39,33 @@ func _populate_settings() -> void:
 	vibrate_checkbox.set_pressed_no_signal(SaveDataManager.save_data.is_vibration_enabled)
 
 
-func _on_vibrate_checkbox_toggled(button_pressed: bool) -> void:
+func _on_day_3_ui_pause_state_changed(is_paused: bool) -> void:
+	show_menu(is_paused)
+
+
+func _on_vibrate_check_button_toggled(button_pressed: bool) -> void:
 	SaveDataManager.save_data.is_vibration_enabled = button_pressed
 	if button_pressed:
 		Utils.vibrate_joy()
-	print(str(button_pressed))
 
 
-func _on_autofire_checkbox_toggled(button_pressed: bool) -> void:
+func _on_autofire_check_button_toggled(button_pressed: bool) -> void:
 	SaveDataManager.save_data.is_autofire_enabled = button_pressed
-	print(str(button_pressed))
 
 
-func _on_give_up_label_pressed() -> void:
+func _on_give_up_button_pressed() -> void:
+	pause_dialog.visible = false
+	confirm_exit_dialog.visible = true
+	no_button.call_deferred("grab_focus")
+
+
+func _on_no_button_pressed() -> void:
+	confirm_exit_dialog.visible = false
+	pause_dialog.visible = true
+	give_up_button.call_deferred("grab_focus")
+
+
+func _on_yes_button_pressed() -> void:
 	SaveDataManager.save()
-	print("YOU HAVE GIVE UP!!")
-
-
-func _on_day_3_ui_pause_state_changed(new_state: bool) -> void:
-	show_menu(new_state)
+	print("YOU GAVE UP!!!!")
+	# TODO return to title screen
