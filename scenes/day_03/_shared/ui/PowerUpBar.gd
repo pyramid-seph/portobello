fa14b7af3672dda64f77e45ac60b12300b2df120
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-01-28 11:25:47 -0600

    Change power up max animation to a tween

--- scenes/day_03/_shared/ui/PowerUpBar.gd
+++ scenes/day_03/_shared/ui/PowerUpBar.gd
@@ -1,16 +1,37 @@
 extends HBoxContainer
 
-const NORMAL_COLOR = Color8(250, 172, 88)
+const NORMAL_COLOR: Color = Color8(250, 172, 88)
 
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
-@onready var progress_bar := $ProgressBar
+var _tween: Tween
+
+@onready var progress_bar := $ProgressBar as TextureProgressBar
 
 
-func _on_Day3Ui_power_up_changed(new_val: float, max_val: float):
-	progress_bar.value = new_val * progress_bar.max_value / max_val
+func _set_progress_color(color: Color) -> void:
+	progress_bar.set_tint_progress(color)
 
-	if new_val == 0:
-		anim_player.stop()
+
+func _cancel_maxed_out_anim() -> void:
+	if _tween: 
+		_tween.kill()
+		_tween = null
 		progress_bar.set_tint_progress(NORMAL_COLOR)
-	elif new_val == max_val:
-		anim_player.play("max_power_up_reached")
+
+
+func _animate_maxed_out() -> void:
+	_cancel_maxed_out_anim()
+	_tween = create_tween()
+	_tween.set_pause_mode(Tween.TWEEN_PAUSE_STOP)
+	_tween.set_loops()
+	_tween.tween_callback(_set_progress_color.bind(Color.RED))
+	_tween.tween_interval(Utils.FRAME_TIME)
+	_tween.tween_callback(_set_progress_color.bind(NORMAL_COLOR))
+	_tween.tween_interval(Utils.FRAME_TIME)
+
+
+func _on_Day3Ui_power_up_changed(new_val: float, max_val: float) -> void:
+	progress_bar.value = new_val * progress_bar.max_value / max_val
+	if new_val < max_val: 
+		_cancel_maxed_out_anim()
+	else:
+		_animate_maxed_out()
