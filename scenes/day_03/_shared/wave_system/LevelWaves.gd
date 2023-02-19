7ed96b23d2c020101f1d0d5fe3e6f6f6578d5b04
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-02-19 14:14:47 -0600

    Change wave movement from string to callable

--- scenes/day_03/_shared/wave_system/LevelWaves.gd
+++ scenes/day_03/_shared/wave_system/LevelWaves.gd
@@ -14,11 +14,11 @@ func _create_wave(
 	enemy_count: int, 
 	time_between_spawns: float,
 	time_between_waves: float,
-	init_move_state_func_name: String
+	init_move_state_func_name: Callable
 ) -> Wave:
 	var wave = Wave.new()
 	wave.enemy_count = enemy_count
 	wave.time_between_spawns = time_between_spawns
 	wave.time_between_waves = time_between_waves
-	wave.get_initial_move_state_func = Callable(self, init_move_state_func_name)
+	wave.get_initial_move_state_func = init_move_state_func_name
 	return wave
