58effd8ba34098ae8da9639c73ae51ba218dca2e
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-05-14 19:15:38 -0600

    Change label background on focus

--- scenes/_shared/title_screen/title_screen_scene.gd
+++ scenes/_shared/title_screen/title_screen_scene.gd
@@ -4,6 +4,7 @@ extends Node
 
 @onready var _title_screen = $TitleScreen
 @onready var _logos_roll := $LogosRoll
+@onready var _story_mode_game_selector = %StoryModeGameSelector
 
 
 func _ready() -> void:
@@ -16,9 +17,29 @@ func _ready() -> void:
 
 func _enable_title_screen(value: bool) -> void:
 	_title_screen.visible = value
-	_title_screen.process_mode = Node.PROCESS_MODE_DISABLED if not value else Node.PROCESS_MODE_ALWAYS
+	if not value:
+		_title_screen.process_mode = Node.PROCESS_MODE_DISABLED 
+	else:
+		_title_screen.process_mode = Node.PROCESS_MODE_ALWAYS
+	_story_mode_game_selector.call_deferred("grab_focus")
 
 
 func _on_logos_roll_rolled() -> void:
 	_logos_roll.visible = false
 	_enable_title_screen(true)
+
+
+func _on_story_mode_game_selector_selected(idx: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_score_attack_game_selector_selected(idx: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_show_scores_btn_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_show_options_btn_pressed() -> void:
+	pass # Replace with function body.
---
+++ scenes/_shared/ui/h_selector.gd
@@ -0,0 +1,134 @@
+@tool
+extends Label
+
+signal selected(idx: int)
+
+const SELECTED_NONE: int = -1
+
+@export var selector_text: String:
+	set(value):
+		selector_text = value
+		_on_selector_text_set()
+@export var options: Array[String]:
+	set(value):
+		options = value
+		_on_options_set()
+@export var focus_color: Color = Color.MAGENTA:
+	set(value):
+		focus_color = value
+		_on_focus_color_set()
+@export var loop_options: bool = true:
+	set(value):
+		loop_options = value
+		_on_loop_options_set()
+
+var _current_option_idx: int = SELECTED_NONE:
+	set(value):
+		_current_option_idx = maxi(value, SELECTED_NONE)
+		_on_current_option_idx_set()
+
+@onready var _is_ready: bool = true
+@onready var _bg_color_rect := $BackgroundColorRect
+
+
+func _init() -> void:
+	var style_box := StyleBoxEmpty.new()
+	add_theme_stylebox_override("normal", StyleBoxFlat.new())
+
+
+func _ready() -> void:
+	_on_options_set()
+	_on_current_option_idx_set()
+	_on_focus_color_set()
+	_update_bg_color()
+
+
+func _gui_input(event: InputEvent) -> void:
+	if not has_focus() or Engine.is_editor_hint():
+		return
+	
+	if event.is_action_pressed("ui_accept"):
+		selected.emit(_current_option_idx)
+		accept_event()
+	if event.is_action_pressed("ui_left"):
+		_previous_option()
+		accept_event()
+	if event.is_action_pressed("ui_right"):
+		_next_option()
+		accept_event()
+
+
+func _previous_option() -> void:
+	if options.is_empty():
+		_current_option_idx = SELECTED_NONE
+		return
+	
+	if loop_options:
+		_current_option_idx = wrapi(_current_option_idx - 1, 0, options.size())
+	else:
+		_current_option_idx = maxi(_current_option_idx - 1, 0)
+
+
+func _next_option() -> void:
+	if options.is_empty():
+		_current_option_idx = SELECTED_NONE
+		return
+	
+	if loop_options:
+		_current_option_idx = wrapi(_current_option_idx + 1, 0, options.size())
+	else:
+		_current_option_idx = mini(options.size() - 1, _current_option_idx + 1)
+
+
+func _update_bg_color() -> void:
+	var curr_theme_style_box := get_theme_stylebox("normal")
+	if has_focus():
+		curr_theme_style_box.bg_color = focus_color
+	else:
+		curr_theme_style_box.bg_color = Color.TRANSPARENT
+
+
+func _on_current_option_idx_set() -> void:
+	if not _is_ready:
+		return
+	
+	text = selector_text
+	if _current_option_idx == SELECTED_NONE:
+		return
+	
+	if options.size() == 1:
+		text += options[0]
+		return
+	
+	if loop_options or _current_option_idx > 0:
+		text += " < "
+	text += options[_current_option_idx]
+	if loop_options or _current_option_idx < options.size() - 1:
+		text += " >"
+
+
+func _on_selector_text_set() -> void:
+	_on_options_set()
+
+
+func _on_options_set() -> void:
+	if not _is_ready:
+		return
+	_current_option_idx = SELECTED_NONE if options.is_empty() else 0
+
+
+func _on_loop_options_set() -> void:
+	_on_current_option_idx_set()
+
+
+func _on_focus_color_set() -> void:
+	if _is_ready:
+		_update_bg_color()
+
+
+func _on_focus_entered() -> void:
+	_update_bg_color()
+
+
+func _on_focus_exited() -> void:
+	_update_bg_color()
