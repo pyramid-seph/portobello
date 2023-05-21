dd0fe938241bd7279952454b1b6350b0d40df1a8
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-05-21 16:02:41 -0600

    Show loadign animation after 1 second

--- autoloads/scene_changer.gd
+++ autoloads/scene_changer.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 
 
 @export var TitleScreen: PackedScene
+@export var _loading_animation_delay_sec: float = 1.0
+
+@onready var _loading_anim_container := $LodingAnimContainer
+@onready var _timer := $Timer as Timer
 
 
 func _ready() -> void:
@@ -10,9 +14,23 @@ func _ready() -> void:
 
 func load_level(game, mode, level) -> void:
 	visible = true
-	# TODO
+	# TODO Load level
+	visible = false
 
 
 func load_title_screen() -> void:
 	visible = true
 	get_tree().change_scene_to_packed(TitleScreen)
+	visible = false
+
+
+func _on_timer_timeout() -> void:
+	_loading_anim_container.visible = true
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		_timer.start(_loading_animation_delay_sec)
+	else:
+		_loading_anim_container.visible = false
+		_timer.stop()
