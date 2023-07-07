c94a34d44379b66097f381ed01df7f5cca369baf
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-07-07 13:43:26 -0600

    Allow player to revive

--- scenes/day_01/player/day_01_player.gd
+++ scenes/day_01/player/day_01_player.gd
@@ -50,9 +50,6 @@ func _process(delta: float) -> void:
 	if _is_dead or stop_moving or Engine.is_editor_hint():
 		return
 	
-	if OS.is_debug_build() and Input.is_action_pressed("fire"):
-		_growth_pending = true
-	
 	_update_next_direction()
 	
 	_elapsed_time_sec += delta
@@ -61,38 +58,53 @@ func _process(delta: float) -> void:
 		_move()
 
 
-func reset() -> void:
-	_elapsed_time_sec = 0.0
-	_curr_dir = INITIAL_DIR
-	_next_dir = _curr_dir
-	_growth_pending = false
-	_is_dead = false
-	_reset_body()
+func _unhandled_input(event: InputEvent) -> void:
+	if not OS.is_debug_build():
+		return
+	if event.is_action_pressed("fire") and not _is_dead:
+		_growth_pending = true
+	elif event.is_action_pressed("debug_revive") and _is_dead:
+		revive()
 
 
 func revive() -> void:
 	if not _is_dead:
 		return
-	_reset_body()
+	_reset()
+
+
+func _is_root_parent() -> bool:
+	return get_parent() == $/root
 
 
 func _reset_body() -> void:
 	for trunk_part in _trunk.get_children():
 		if trunk_part != _first_trunk_part:
 			trunk_part.queue_free()
-	_head.global_position = DEBUG_POS if get_parent() == $/root else global_position
+	_head.global_position = DEBUG_POS if _is_root_parent() else global_position
+	_head.rotation = Vector2(_curr_dir).angle()
 	_first_trunk_part.position.x = _head.position.x - _pixels_per_step
 	_first_trunk_part.position.y = _head.position.y
-	_tail.position.x = _head.position.x - _pixels_per_step * (_trunk.get_child_count() + 1)
-	_tail.position.y = _head.position.y
-	_head.visible = true
+	_first_trunk_part.rotation = _head.rotation
+	_tail.position.x = _first_trunk_part.position.x - _pixels_per_step
+	_tail.position.y = _first_trunk_part.position.y
+	_tail.rotation = _head.rotation
 	_dead_head.visible = false
-	_first_trunk_part.visible = true
 	if Engine.is_editor_hint():
 		_tail.stop()
 		_head.stop()
 	else:
 		_tail.play("default")
+		_head.play("default")
+
+
+func _reset() -> void:
+	_elapsed_time_sec = 0.0
+	_curr_dir = INITIAL_DIR
+	_next_dir = _curr_dir
+	_growth_pending = false
+	_is_dead = false
+	_reset_body()
 
 
 func _update_next_direction() -> void:
@@ -170,6 +182,5 @@ func _on_died() -> void:
 	_dead_head.visible = true
 	_dead_head.position = _first_trunk_part.position
 	_dead_head.rotation = _head.rotation
-	_first_trunk_part.visible = false
-	_head.visible = false
 	_tail.stop()
+	_head.stop()
