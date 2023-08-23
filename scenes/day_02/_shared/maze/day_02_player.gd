a08ffe83f212a6d6234912f3b5a97461aed6b079
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-08-22 19:53:50 -0600

    Redraw some sprites

--- scenes/day_02/_shared/maze/day_02_player.gd
+++ scenes/day_02/_shared/maze/day_02_player.gd
@@ -1,55 +1,31 @@
 extends Area2D
 
+const SPEED: float = 50.0 # 4 pixels every 0.08 seconds (OG game -> 1 frame = 0.08s)
+const INPUT_TOLERANCE: float = 0.4
+const TILE_SIZE: int = 16
 
-var _maze: TileMap
-var _curr_dir: Vector2i = Vector2i.ZERO
-var _candidate_curr: Vector2i = Vector2i.ZERO
-var _next_input: Vector2i
+var _curr_dir: Vector2i
+var _candidate_curr: Vector2i
+var _target_position: Vector2
 
 @onready var _animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var _timer := $Timer as Timer
-
-
-func _ready() -> void:
-	_maze = get_parent() as TileMap
+@onready var _maze := get_parent() as TileMap
 
 
 func _physics_process(delta: float) -> void:
-	if not _timer.is_stopped():
+	if _candidate_curr == Vector2i.ZERO and _curr_dir == Vector2i.ZERO:
 		return
-	else:
-		_timer.start()
 	
 	var candidate_works := _candidate_curr != Vector2i.ZERO and _maze.get_cell_source_id(0, _map_pos() + _candidate_curr) == -1
 	var curr_works = _maze.get_cell_source_id(0, _map_pos() + _curr_dir) == -1
 	if candidate_works:
-		print("candidate_works")
 		_curr_dir = _candidate_curr
 	elif not candidate_works and not curr_works:
-		print("NO")
 		_curr_dir = Vector2i.ZERO
-	else:
-		print("curr_works")
 	var next_map_pos := _map_pos() + _curr_dir
-	position = _maze.map_to_local(next_map_pos)
 	
-	match _curr_dir:
-		Vector2i.UP:
-			_animated_sprite.rotation_degrees = 90.0
-			_animated_sprite.flip_h = true
-			_animated_sprite.flip_v = false
-		Vector2i.LEFT:
-			_animated_sprite.rotation_degrees = 0.0
-			_animated_sprite.flip_h = true
-			_animated_sprite.flip_v = false
-		Vector2i.RIGHT:
-			_animated_sprite.rotation_degrees = 0.0
-			_animated_sprite.flip_h = false
-			_animated_sprite.flip_v = false
-		Vector2i.DOWN:
-			_animated_sprite.rotation_degrees = 90.0
-			_animated_sprite.flip_h = false
-			_animated_sprite.flip_v = true
+	_update_sprite_direction()
 	
 	_candidate_curr = Vector2.ZERO
 
@@ -65,8 +41,29 @@ func _unhandled_input(event: InputEvent) -> void:
 		_candidate_curr = Vector2i.RIGHT
 
 
-func _snap_pos() -> void:
-	position = _maze.map_to_local(_map_pos())
+func _get_target_pos() -> Vector2:
+	var target_pos: Vector2 = Vector2.ZERO
+	return target_pos
+
+
+func _update_sprite_direction() -> void:
+	match _curr_dir:
+		Vector2i.UP:
+			_animated_sprite.rotation_degrees = 90.0
+			_animated_sprite.flip_h = true
+			_animated_sprite.flip_v = false
+		Vector2i.LEFT:
+			_animated_sprite.rotation_degrees = 0.0
+			_animated_sprite.flip_h = true
+			_animated_sprite.flip_v = false
+		Vector2i.DOWN:
+			_animated_sprite.rotation_degrees = 90.0
+			_animated_sprite.flip_h = false
+			_animated_sprite.flip_v = true
+		_:
+			_animated_sprite.rotation_degrees = 0.0
+			_animated_sprite.flip_h = false
+			_animated_sprite.flip_v = false
 
 
 func _map_pos() -> Vector2i:
