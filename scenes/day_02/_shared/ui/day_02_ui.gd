8b5090acae85bdc5aee96560d77982d648e865f1
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-11-12 11:42:48 -0600

    Change floating text line spacing

--- scenes/day_02/_shared/ui/day_02_ui.gd
+++ scenes/day_02/_shared/ui/day_02_ui.gd
@@ -32,7 +32,7 @@ func show_level_start(mode: Game.Mode, index: int) -> void:
 func show_game_over(new_high_score: bool) -> void:
 	_game_over.text = "¡Qué indigestión!"
 	if new_high_score:
-		_game_over.text += "\n¡Nuevo récord!"
+		_game_over.text += "\n\n¡Nuevo récord!"
 	_game_over.start()
 	await _game_over.finished
 	game_over_finished.emit()
