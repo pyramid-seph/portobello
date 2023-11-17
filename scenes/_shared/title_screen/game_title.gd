d799c19313b7d7f9db1a831c567881bf837c46fb
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-11-16 21:51:33 -0600

    Change title after *that* cutscene

--- scenes/_shared/title_screen/game_title.gd
+++ scenes/_shared/title_screen/game_title.gd
@@ -1,6 +1,8 @@
 extends TextureRect
 
 
+@export var normal_title: Texture2D
+@export var bloody_title: Texture2D
 @export var stars_count: int:
 	set(value):
 		var old_value = stars_count
@@ -16,6 +18,10 @@ func _ready() -> void:
 	_on_stars_count_changed()
 
 
+func set_story_mode_progress(max_day_completed: int) -> void:
+	texture = normal_title if max_day_completed < 2 else bloody_title
+
+
 func _on_stars_count_changed() -> void:
 	if not _is_ready:
 		return
@@ -24,4 +30,3 @@ func _on_stars_count_changed() -> void:
 	for i in stars_count:
 		stars_text += "*"
 	_stars_label.text = stars_text
-
