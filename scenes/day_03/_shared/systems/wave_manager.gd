d014b6e37c0f414384ef040dc2ee0175693ccab9
pyramid-seph <pyramid-seph@users.noreply.github.com>
2023-12-02 17:55:44 -0600

    Refactor wave manager
    The old implementation leaked Wave objects.

--- scenes/day_03/_shared/systems/wave_manager.gd
+++ scenes/day_03/_shared/systems/wave_manager.gd
@@ -9,13 +9,19 @@ signal wave_started(wave_index)
 const MAX_CONCURRENT_ENEMIES: int = 10
 
 var _waves_descriptor: LevelWaves
-
 var _spawned_enemies_count: int = 0
 var _enemies_on_screen: int = 0
-var _is_canceled: bool = false
+var _is_stopped: bool = true
+var _world: Node2D
+var _player: Day03Player
+var _wave_index: int = -1
+var _wave_enemy_index: int
+var _wave_memo: Dictionary = {}
 
 @onready var _screen_size: Vector2 = get_viewport_rect().size
-@onready var _timer := $Timer as Timer
+@onready var _wave_finished_check_timer: Timer = $WaveFinishedCheckTimer
+@onready var _wave_delay_timer: Timer = $WaveDelayTimer
+@onready var _spawn_delay_timer: Timer = $SpawnDelayTimer
 
 
 func _ready() -> void:
@@ -24,84 +30,137 @@ func _ready() -> void:
 	)
 
 
+func _exit_tree() -> void:
+	print(" \n========\n ")
+
+
 func start(world: Node2D, player: Day03Player) -> void:
-	_is_canceled = false
+	if not _is_stopped:
+		return
 	
 	if not _pre_start_checks_passed(world, player):
+		stop()
 		all_waves_completed.emit()
 		return
 	
-	var wave_index = -1
-	for wave: Wave  in _waves_descriptor.get_waves():
-		var wave_enemy_index = 0
-		var wave_memo = {}
-		_spawned_enemies_count = 0
-		_enemies_on_screen = 0
-		wave_index += 1
-		
-		wave_started.emit(wave_index)
-		
-		_timer.start(wave.time_between_spawns)
-		await _timer.timeout
-		while _spawned_enemies_count < wave.enemy_count and not _is_canceled:
-			if _enemies_on_screen >= MAX_CONCURRENT_ENEMIES:
-				_timer.start(Utils.FRAME_TIME)
-				await _timer.timeout
-				continue
-			
-			var enemy = wave.Enemy.instantiate()
-			var movement = wave.calculate_pattern.call(
-				wave_enemy_index,
-				player.global_position,
-				_screen_size,
-				wave_memo
-			)
-			enemy.global_position = movement.initial_global_position
-			enemy.movement_pattern = movement.pattern
-			enemy.tree_exited.connect(_on_enemy_tree_exited, CONNECT_ONE_SHOT)
-			world.add_child(enemy)
-			
-			_enemies_on_screen += 1
-			_spawned_enemies_count += 1
-			wave_enemy_index += 1
-			
-			_timer.start(wave.time_between_spawns)
-			await _timer.timeout
-		
-		while _enemies_on_screen > 0 and not _is_canceled:
-			_timer.start(Utils.FRAME_TIME)
-			await _timer.timeout
-		
-		_timer.start(wave.time_between_waves)
-		await _timer.timeout
-		
-		wave_completed.emit(wave_index)
-		
-	if not _is_canceled:
-		all_waves_completed.emit()
+	_wave_index = -1
+	_is_stopped = false
+	_world = world
+	_player =  player
+	_start_next_wave()
 
 
-func cancel_wave() -> void:
-	_is_canceled = true
+func stop() -> void:
+	_is_stopped = true
+	_world = null
+	_player = null
+	_wave_delay_timer.stop()
+	_spawn_delay_timer.stop()
+	_wave_finished_check_timer.stop()
+	print("Wave manager has been stopped")
+
+
+func _get_current_wave() -> Wave:
+	var waves: Array[Wave] = _waves_descriptor.get_waves()
+	return null if _wave_index < 0 else waves[_wave_index]
+
+
+func _start_next_wave() -> void:
+	if _is_stopped:
+		return
+	
+	_wave_enemy_index = 0
+	_wave_memo.clear()
+	_spawned_enemies_count = 0
+	_enemies_on_screen = 0
+	_wave_index += 1
+	var wave: Wave = _get_current_wave()
+	_spawn_delay_timer.start(wave.time_between_spawns)
+	wave_started.emit(_wave_index)
+	print("Wave started: ", _wave_index)
 
 
 func _pre_start_checks_passed(world: Node2D, player: Day03Player) -> bool:
 	if world == null:
-		print("Completing wave phase  because world cannot be null.")
+		print("Completing _wave phase  because world cannot be null.")
 		return false
 	
 	if player == null:
-		print("Completing wave phase  because player cannot be null.")
+		print("Completing _wave phase  because player cannot be null.")
 		return false
 	
 	if _waves_descriptor == null:
-		print("Completing wave phase because level waves descriptor cannot be null.")
+		print("Completing _wave phase because level waves descriptor cannot be null.")
 		return false
 	
 	return true
 
 
+func _spawn_enemy() -> void:
+	var wave: Wave = _get_current_wave()
+	var enemy = wave.Enemy.instantiate()
+	var movement = wave.calculate_pattern.call(
+		_wave_enemy_index,
+		_player.global_position,
+		_screen_size,
+		_wave_memo
+	)
+	enemy.global_position = movement.initial_global_position
+	enemy.movement_pattern = movement.pattern
+	enemy.tree_exited.connect(_on_enemy_tree_exited, CONNECT_ONE_SHOT)
+	_world.add_child(enemy)
+	
+	_enemies_on_screen += 1
+	_spawned_enemies_count += 1
+	_wave_enemy_index += 1
+
+
 func _on_enemy_tree_exited() -> void:
-	if _is_canceled:
+	if _is_stopped:
 		return
 	_enemies_on_screen = maxi(_enemies_on_screen - 1, 0)
+
+
+func _on_wave_finished_check_timer_timeout() -> void:
+	if _is_stopped:
+		return
+	
+	if _enemies_on_screen <= 0:
+		var wave: Wave = _get_current_wave()
+		_wave_delay_timer.start(wave.time_between_waves)
+	else:
+		_wave_finished_check_timer.start(Utils.FRAME_TIME)
+
+
+func _on_spawn_delay_timer_timeout() -> void:
+	if _is_stopped:
+		return
+	
+	var wave: Wave = _get_current_wave()
+	if _spawned_enemies_count >= wave.enemy_count:
+		_on_wave_finished_check_timer_timeout()
+		return
+	
+	if _enemies_on_screen >= MAX_CONCURRENT_ENEMIES:
+		_spawn_delay_timer.start(Utils.FRAME_TIME)
+		return
+	
+	_spawn_enemy()
+	
+	_spawn_delay_timer.start(wave.time_between_spawns)
+
+
+func _on_wave_delay_timer_timeout() -> void:
+	if _is_stopped:
+		return
+	
+	print("_spawned_enemies_count: ", _spawned_enemies_count)
+	
+	if _wave_index >= _waves_descriptor.get_waves().size() - 1:
+		stop()
+		print("All waves completed")
+		all_waves_completed.emit()
+	else:
+		print("Wave completed: ", _wave_index)
+		wave_completed.emit(_wave_index)
+		_start_next_wave()
