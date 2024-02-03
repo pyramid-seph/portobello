9afd5c0b6a3a50ee30271c58e33e41caeb1313c9
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-02-02 19:52:35 -0600

    Change touch screen controller visibility when a physical controller is connected/disconnected

--- autoloads/touch_controller_manager.gd
+++ autoloads/touch_controller_manager.gd
@@ -18,6 +18,7 @@ var mode: Mode:
 	set(value):
 		mode = value
 		_on_mode_set()
+
 var _touch_screen_controller: TouchScreenController
 
 
@@ -29,6 +30,12 @@ func _ready() -> void:
 	if DisplayServer.is_touchscreen_available():
 		_touch_screen_controller = TouchScreenControllerScn.instantiate()
 		add_child(_touch_screen_controller)
+	
+		Input.joy_connection_changed.connect(_on_joy_connection_changed)
+		# Doesn't seem to be necessary to call _update_touch_controller_visibility here.
+		# _on_joy_connection_changed is called when the game starts (on desktop)
+		# or when the user presses a button or moves a stick for the first time
+		# after the game starts (on web).
 
 
 func _configure_controller_for_mode(new_mode: Mode) -> void:
@@ -37,7 +44,8 @@ func _configure_controller_for_mode(new_mode: Mode) -> void:
 	
 	match new_mode:
 		Mode.UI_MENU:
-			_touch_screen_controller.main_action_button_texture = _button_action_main_accept_texture
+			_touch_screen_controller.main_action_button_texture = \
+					_button_action_main_accept_texture
 			_touch_screen_controller.action_button = &"ui_accept"
 			_touch_screen_controller.action_down = &"ui_down"
 			_touch_screen_controller.action_left = &"ui_left"
@@ -46,7 +54,8 @@ func _configure_controller_for_mode(new_mode: Mode) -> void:
 			_touch_screen_controller.hide_main_action_button = false
 			_touch_screen_controller.hide_pause_button = true
 		Mode.CUTSCENE:
-			_touch_screen_controller.main_action_button_texture = _button_action_main_skip_texture
+			_touch_screen_controller.main_action_button_texture = \
+					_button_action_main_skip_texture
 			_touch_screen_controller.action_button = &"skip_cutscene"
 			_touch_screen_controller.action_down = &"ui_down"
 			_touch_screen_controller.action_left = &"ui_left"
@@ -55,7 +64,8 @@ func _configure_controller_for_mode(new_mode: Mode) -> void:
 			_touch_screen_controller.hide_main_action_button = false
 			_touch_screen_controller.hide_pause_button = true
 		Mode.GAMEPLAY:
-			_touch_screen_controller.main_action_button_texture = _button_action_main_fire_texture
+			_touch_screen_controller.main_action_button_texture = \
+					_button_action_main_fire_texture
 			_touch_screen_controller.action_button = &"fire"
 			_touch_screen_controller.action_down = &"move_down"
 			_touch_screen_controller.action_left = &"move_left"
@@ -65,6 +75,7 @@ func _configure_controller_for_mode(new_mode: Mode) -> void:
 			_touch_screen_controller.hide_pause_button = false
 		Mode.GAMEPLAY_DPAD_ONLY:
 			_touch_screen_controller.main_action_button_texture = null
+			# action_button cannot be null, so I'll just set it to whatever...
 			_touch_screen_controller.action_button = &"fire"
 			_touch_screen_controller.action_down = &"move_down"
 			_touch_screen_controller.action_left = &"move_left"
@@ -74,6 +85,19 @@ func _configure_controller_for_mode(new_mode: Mode) -> void:
 			_touch_screen_controller.hide_pause_button = false
 
 
+func _is_any_joypad_connected() -> bool:
+	return not Input.get_connected_joypads().is_empty()
+
+
+func _update_touch_controller_visibility() -> void:
+	if _touch_screen_controller:
+		_touch_screen_controller.visible = not _is_any_joypad_connected()
+
+
 func _on_mode_set() -> void:
 	if is_node_ready():
 		_configure_controller_for_mode(mode)
+
+
+func _on_joy_connection_changed(_device: int, _connected: bool) -> void:
+	_update_touch_controller_visibility()
