9304909b56e1d2c07490e66077c6c0b42e328aef
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-06-01 16:44:35 -0600

    Change the visibility of a group of controls without needing them to be children of another node

---
+++ scenes/day_ex/ui/group.gd
@@ -0,0 +1,32 @@
+@tool
+## Controls the visibility of the referenced Controls
+class_name GroupControl
+extends Control
+
+
+@export var controls: Array[Control]:
+	set(value):
+		controls = value
+		_update_referenced_controls_visibility()
+@export var referenced_controls_visibility: bool = true:
+	set(value):
+		referenced_controls_visibility = value
+		_update_referenced_controls_visibility()
+
+
+func _ready() -> void:
+	_on_visibility_changed()
+	visibility_changed.connect(_on_visibility_changed)
+
+
+func _update_referenced_controls_visibility() -> void:
+	if not is_node_ready():
+		return
+	
+	for control: Control in controls:
+		if control != null and is_instance_valid(control):
+			control.visible = referenced_controls_visibility
+
+
+func _on_visibility_changed() -> void:
+	hide()
