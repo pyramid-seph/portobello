25e62ad19ccdbe1c56459036f596f1fe9525af29
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-06-02 18:14:16 -0600

    Add visibility changed signal to GroupVisibility

--- scenes/day_ex/ui/group_visibility.gd
+++ scenes/day_ex/ui/group_visibility.gd
@@ -3,6 +3,7 @@
 class_name GroupVisibility
 extends Control
 
+signal referenced_controls_visibility_changed
 
 @export var controls: Array[Control]:
 	set(value):
@@ -10,13 +11,16 @@ extends Control
 		_update_referenced_controls_visibility()
 @export var referenced_controls_visibility: bool = true:
 	set(value):
+		var old_value: bool = referenced_controls_visibility
 		referenced_controls_visibility = value
 		_update_referenced_controls_visibility()
+		if old_value != value:
+			_on_referenced_controls_visibility_changed()
 
 
 func _ready() -> void:
-	_on_visibility_changed()
-	visibility_changed.connect(_on_visibility_changed)
+	_on_self_visibility_changed()
+	visibility_changed.connect(_on_self_visibility_changed)
 
 
 func _update_referenced_controls_visibility() -> void:
@@ -26,7 +30,13 @@ func _update_referenced_controls_visibility() -> void:
 	for control: Control in controls:
 		if control != null and is_instance_valid(control):
 			control.visible = referenced_controls_visibility
+	referenced_controls_visibility_changed.emit()
 
 
-func _on_visibility_changed() -> void:
+func _on_referenced_controls_visibility_changed() -> void:
+	if is_node_ready():
+		referenced_controls_visibility_changed.emit()
+
+
+func _on_self_visibility_changed() -> void:
 	hide()
