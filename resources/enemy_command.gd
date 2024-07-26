fd693387986053c84aa6e2c8cb8addfe7ca5a150
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-07-25 19:51:22 -0600

    Fix default command weight

--- resources/enemy_command.gd
+++ resources/enemy_command.gd
@@ -2,7 +2,7 @@ class_name EnemyCommand
 extends Resource
 
 @export var _action: BattleAction
-@export_range(1.0, 100.0, 1.0, "hide_slider") var _weight: float
+@export_range(1.0, 100.0, 1.0, "hide_slider") var _weight: float = 1.0
 
 
 func get_action() -> BattleAction:
