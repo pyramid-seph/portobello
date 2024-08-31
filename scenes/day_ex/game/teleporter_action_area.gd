940d7832973dfb1331a0bc68d68239bb70da19b2
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-08-30 19:04:45 -0600

    Teleport player to the boss area and back

---
+++ scenes/day_ex/game/teleporter_action_area.gd
@@ -0,0 +1,37 @@
+@tool
+extends ActionArea
+
+
+const Player = preload("res://scenes/day_ex/player/day_ex_player.gd")
+
+@export var initial_direction: Player.FacingDirection
+@export var _exit: Marker2D:
+	set(value):
+		_exit = value
+		update_configuration_warnings()
+
+var _teleporting: bool
+
+@onready var _timer: Timer = $Timer
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: Array[String] = []
+	if not _exit:
+		warnings.append("This teleporter has no exit.")
+	return warnings
+
+
+func execute(target: CharacterBody2D) -> void:
+	var player := target as Player
+	if player and _exit and not _teleporting:
+		_teleporting = true
+		player.set_process_unhandled_input(false)
+		await TransitionPlayer.play_default()
+		var offset: Vector2 = player.global_position - global_position
+		player.teleport(_exit.global_position + offset, initial_direction)
+		_timer.start(1.0)
+		await _timer.timeout
+		await TransitionPlayer.play_default_backwards()
+		player.set_process_unhandled_input(true)
+		_teleporting = false
