08959aa155d396006fd52fa219fdf8fcb344b479
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-10-14 19:30:53 -0600

    Change some images

--- scenes/day_ex/game/ending_cutscene_player.gd
+++ scenes/day_ex/game/ending_cutscene_player.gd
@@ -12,13 +12,13 @@ const DummyPlayerScene = preload("res://scenes/day_ex/player/day_ex_player_dummy
 @export var _world: Node2D
 @export var _bird_fighters_path: Array[NodePath]
 @export var _rest_of_bird_gang_path: Array[NodePath]
-@export var _hen_path: NodePath
+@export var _last_duck_path: NodePath
 @export var _boss_final_words: DialogueEvent
 @export var _ending_dialogue: DialogueEvent
 
 var _bird_fighters: Array[Npc]
 var _rest_of_bird_gang: Array[Npc]
-var _hen: Npc
+var _last_duck: Npc
 
 @onready var _ui: DayExUi = %DayExUi
 @onready var _player: DayExPlayer = %DayExPlayer
@@ -34,7 +34,7 @@ func _ready() -> void:
 	for i: int in _rest_of_bird_gang_path.size():
 		_rest_of_bird_gang[i] = get_node(_rest_of_bird_gang_path[i]) as Npc
 	
-	_hen = get_node(_hen_path) as Npc
+	_last_duck = get_node(_last_duck_path) as Npc
 
 
 func play() -> void:
@@ -47,7 +47,7 @@ func play() -> void:
 	await _suspend_bucho_eats_fighters()
 	await _suspend_bucho_scares_everyone_else()
 	await _suspend_bucho_eats_rest_of_the_ducks()
-	await _suspend_bucho_eats_hen()
+	await _suspend_bucho_eats_last_duck()
 	await _suspend_bucho_takes_flight()
 	await _suspend_narrator()
 	
@@ -86,7 +86,7 @@ func _suspend_bucho_scares_everyone_else() -> void:
 	await _timer.timeout
 	for npc: Npc in _rest_of_bird_gang:
 		npc.get_scared(true)
-	_hen.get_scared(true)
+	_last_duck.get_scared(true)
 
 
 func _suspend_bucho_eats_rest_of_the_ducks() -> void:
@@ -105,13 +105,13 @@ func _suspend_bucho_eats_rest_of_the_ducks() -> void:
 	_rest_of_bird_gang.clear()
 
 
-func _suspend_bucho_eats_hen() -> void:
+func _suspend_bucho_eats_last_duck() -> void:
 	_timer.start(2.0)
 	await _timer.timeout
 	_ui.show_black_screen(true)
-	_player.teleport(_hen.global_position, DayExPlayer.FacingDirection.DOWN)
-	_hen.die()
-	_hen = null
+	_player.teleport(_last_duck.global_position, DayExPlayer.FacingDirection.DOWN)
+	_last_duck.die()
+	_last_duck = null
 	_timer.start(0.5)
 	await _timer.timeout
 	_ui.show_black_screen(false)
@@ -136,7 +136,7 @@ func _suspend_bucho_takes_flight() -> void:
 	var dummy_global_pos: Vector2 = dummy.global_position
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(dummy, "global_position:y", dummy_global_pos.y - 100.0,
+	tween.tween_property(dummy, "global_position:y", dummy_global_pos.y - 125.0,
 			2.0)
 	tween.tween_interval(2.0)
 	tween.set_trans(Tween.TRANS_LINEAR)
