f205afabdff0169f71a4b81a69e872fac142f8e1
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-11-14 18:18:59 -0600

    Update licenses

--- scenes/_shared/ui/results_screen.gd
+++ scenes/_shared/ui/results_screen.gd
@@ -4,19 +4,18 @@ extends Control
 signal calculated(new_high_score: int, stars: int)
 signal finished(total_score: int, extra_lives: int, stars: int)
 
-const _results_screen_bgm: AudioStream = preload("res://audio/bgm/bgm_results_screen_theme.wav")
-const _score_calculation_sound: AudioStream = preload("res://audio/sfx/sfx_results_screen_total_score_calculation.wav")
-const _extra_lives_sound: AudioStream = preload("res://audio/sfx/sfx_results_screen_total_score_extra_lives.wav")
-const _stars_count_zero_sound: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_count_zero.wav")
-const _stars_count_sound: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_count.wav")
-const _stars_result_good_sound: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_result_good.wav")
-const _stars_result_best_sound: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_result_best.wav")
-const _stars_result_bad_sound: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_result_bad.wav")
-
 enum StarsEvaluationMode {
 	LIVES,
 	SCORE,
 }
+
+const SFX_CALCULATION: AudioStream = preload("res://audio/sfx/sfx_results_screen_total_score_calculation.wav")
+const SFX_EXTRA_LIVES: AudioStream = preload("res://audio/sfx/sfx_results_screen_total_score_extra_lives.wav")
+const SFX_STARS_COUNT_ZERO: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_count_zero.wav")
+const SFX_STARS_COUNT: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_count.wav")
+const SFX_STARS_RESULT_GOOD: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_result_good.wav")
+const SFX_STARS_RESULT_BEST: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_result_best.wav")
+const SFX_STARS_RESULT_BAD: AudioStream = preload("res://audio/sfx/sfx_results_screen_stars_result_bad.wav")
 
 const BONUS_MULTIPLIER: int = 1000
 const MAX_LIVES: int = 9
@@ -69,7 +68,8 @@ func _ready() -> void:
 	_ensure_reset_ui()
 
 
-func start(game_mode: Game.Mode, is_last_level: bool, lives: int, score: int, high_score: int) -> void:
+func start(game_mode: Game.Mode, is_last_level: bool, lives: int, score: int,
+		high_score: int) -> void:
 	_lives = lives
 	_score = score
 	_curr_high_score = high_score
@@ -181,24 +181,24 @@ func _tween_level_results() -> void:
 	_tween.tween_interval(LEVEL_RESULTS_INITIAL_INTERVAL_SEC)
 	_tween.tween_callback(func(): 
 		Utils.change_label_color(_score_label, RESULTS_LABELS_COLOR)
-		SoundManager.play_sound(_score_calculation_sound)
+		SoundManager.play_sound(SFX_CALCULATION)
 	)
 	_tween.tween_interval(LEVEL_RESULTS_LABELS_DELAY_SEC)
 	_tween.tween_callback(func(): 
 		Utils.change_label_color(_lives_bonus_label, RESULTS_LABELS_COLOR)
-		SoundManager.play_sound(_score_calculation_sound)
+		SoundManager.play_sound(SFX_CALCULATION)
 	)
 	_tween.tween_interval(LEVEL_RESULTS_LABELS_DELAY_SEC)
 	_tween.tween_callback(func(): 
 		Utils.change_label_color(_total_score_label, RESULTS_LABELS_COLOR)
-		SoundManager.play_sound(_score_calculation_sound)
+		SoundManager.play_sound(SFX_CALCULATION)
 	)
 	_tween.tween_interval(LEVEL_RESULTS_LABELS_DELAY_SEC)
 	if _extra_lives > 0:
 		_tween.tween_interval(LEVEL_RESULTS_LABELS_DELAY_SEC)
 		_tween.tween_callback(func(): 
 			Utils.change_label_color(_extra_lives_label, RESULTS_LABELS_COLOR)
-			SoundManager.play_sound(_extra_lives_sound)
+			SoundManager.play_sound(SFX_EXTRA_LIVES)
 		)
 	_tween.tween_interval(LEVEL_RESULTS_LAST_INTERVAL_SEC)
 	_tween.tween_callback(func(): _results_container.visible = false)
@@ -215,7 +215,8 @@ func _tween_minigame_results() -> void:
 			high_score_label_color = COMPLETE_LABELS_COLOR
 		else:
 			high_score_label_color = Color.TRANSPARENT
-		Utils.change_label_color(_new_curr_high_score_label, high_score_label_color)
+		Utils.change_label_color(_new_curr_high_score_label,
+				high_score_label_color)
 	)
 	_tween.tween_interval(LEVEL_COMPLETED_DURATION_SEC)
 	_tween.tween_callback(func():
@@ -236,23 +237,23 @@ func _tween_stars_results() -> void:
 	if _stars < 1:
 		_tween.tween_callback(func():
 			_stars_label.text = "RESULTS_SCREEN_EVALUATION_RESULT_ZERO"
-			SoundManager.play_sound(_stars_count_zero_sound)
+			SoundManager.play_sound(SFX_STARS_COUNT_ZERO)
 		)
 		_tween.tween_interval(STARS_DURATION_SEC)
 	else:
 		for i: int in _stars:
 			_tween.tween_callback(func():
 				_stars_label.text += "*" if i == 0 else " *"
-				SoundManager.play_sound(_stars_count_sound)
+				SoundManager.play_sound(SFX_STARS_COUNT)
 			)
 			_tween.tween_interval(STARS_DURATION_SEC)
 	_tween.tween_callback(func():
 		_evaluation_label.visible = true
-		var stars_result_sound: AudioStream = _stars_result_good_sound
+		var stars_result_sound: AudioStream = SFX_STARS_RESULT_GOOD
 		if _stars < 2:
-			stars_result_sound = _stars_result_bad_sound
+			stars_result_sound = SFX_STARS_RESULT_BAD
 		elif _stars == 5:
-			stars_result_sound = _stars_result_best_sound
+			stars_result_sound = SFX_STARS_RESULT_BEST
 		SoundManager.play_sound(stars_result_sound)
 	)
 	_tween.tween_interval(STARS_LAST_INTERVAL_SEC)
