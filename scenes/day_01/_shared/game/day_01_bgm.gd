9a8711730ef977052746bc07a203c372eaabf205
pyramid-seph <pyramid-seph@users.noreply.github.com>
2024-11-18 10:55:35 -0600

    Change implementation of day 01 BGM player

--- scenes/day_01/_shared/game/day_01_bgm.gd
+++ scenes/day_01/_shared/game/day_01_bgm.gd
@@ -3,15 +3,15 @@ extends Node
 
 const Day01Player = preload("res://scenes/day_01/player/day_01_player.gd")
 
-const DAY_01_BGM_STREAM = preload("res://resources/instances/day_01_bgm_stream.tres")
+const BGM_DAY_01_INTRO = preload("res://audio/bgm/remaki_1.wav")
+const BGM_DAY_01_MAIN = preload("res://audio/bgm/remaki_0.wav")
+const BGM_DAY_01_SUCCESS_FANFARE = preload("res://audio/bgm/bgm_day_01_success_fanfare.wav")
 
-var _playback: AudioStreamPlaybackInteractive
-
-@onready var player: Day01Player = %Day01Player
+@onready var _player: Day01Player = %Day01Player
 
 
 func _ready() -> void:
-	player.started_moving.connect(_on_day_01_player_started_moving)
+	_player.started_moving.connect(_on_day_01_player_started_moving)
 
 
 func _exit_tree() -> void:
@@ -19,53 +19,30 @@ func _exit_tree() -> void:
 
 
 func play_level_music() -> void:
-	await _suspend_ensure_playing()
-	if player.is_awaiting_first_movement():
+	if _player.is_awaiting_first_movement():
 		_switch_to_intro()
 	else:
 		_switch_to_main()
 
 
 func play_success_music() -> void:
-	await _suspend_ensure_playing()
 	_switch_to_success()
 
 
 func stop_music() -> void:
-	_switch_to_intro()
-	_playback = null
-	
-	if SoundManager.is_music_playing(DAY_01_BGM_STREAM):
-		SoundManager.stop_music()
-
-
-func _suspend_ensure_playing() -> void:
-	if SoundManager.is_music_playing(DAY_01_BGM_STREAM):
-		return
-	
-	var audio_player: AudioStreamPlayer = \
-			SoundManager.play_music(DAY_01_BGM_STREAM)
-	# Waiting for the next process frame so I don't get an error 
-	# when trying to access the AudioStreamPlaybackInteractive.
-	await get_tree().process_frame
-	if SoundManager.is_music_playing(DAY_01_BGM_STREAM):
-		_playback = \
-				audio_player.get_stream_playback() as AudioStreamPlaybackInteractive
+	SoundManager.stop_music()
 
 
 func _switch_to_intro() -> void:
-	if _playback:
-		_playback.switch_to_clip_by_name(&"intro")
+	SoundManager.play_music(BGM_DAY_01_INTRO)
 
 
 func _switch_to_main() -> void:
-	if _playback:
-		_playback.switch_to_clip_by_name(&"main")
+	SoundManager.play_music(BGM_DAY_01_MAIN)
 
 
 func _switch_to_success() -> void:
-	if _playback:
-		_playback.switch_to_clip_by_name(&"success")
+	SoundManager.play_music(BGM_DAY_01_SUCCESS_FANFARE)
 
 
 func _on_day_01_player_started_moving() -> void:
